5d0fab3c8395061c664cf0d4a85a312a19ace6c2
xhoch3.info <xhoch3.info@gmail.com>
2018-10-22 16:01:15 +0200

    Switch-Method of Gallery changed to $get.[fragment].html within <ul></ul>

--- js/myGallery.js
+++ js/myGallery.js
@@ -4,11 +4,11 @@
  * and open the template in the editor.
  */
 
-$(document).ready(function(){
+//$(document).ready(function(){
     $('#get').on('click',function(){
         $.getScript('js/loadJsonZimmer.js');
     });
-    $('#JSON').on('click',function(){
+    $('#get').on('click',function(){
         $.getScript('js/loadJsonHotel.js');
     });
-});
+//});
